cb8b209c5f3c7b5b4b04e4d2e44455d90472c54e
CMGS <ilskdw@gmail.com>
2011-11-20 11:05:55 +0800

    add logic

---
+++ alloc.py
@@ -0,0 +1,27 @@
+#!/opt/local/bin/python2.7
+#coding:utf-8
+
+import MySQLdb
+
+def generate_app_uid(appname):
+    conn = MySQLdb.connect()
+    cur = conn.cursor()
+    sql = r'''INSERT INTO `apps` (`app_name`) VALUES (%s)'''
+    cur.execute(sql, (appname,))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return get_app_uid(appname)
+
+def get_app_uid(appname):
+    conn = MySQLdb.connect()
+    cur = conn.cursor()
+    sql = r'''SELECT app_uid FROM apps WHERE app_name = %s'''
+    cur.execute(sql, (appname,))
+    result = cur.fetchall()
+    cur.close()
+    conn.close()
+    if len(result) > 0:
+        return result[0][0]
+    return generate_app_uid(appname)
+    
---
+++ syncdb.py
@@ -0,0 +1,40 @@
+#!/opt/local/bin/python2.7
+#coding:utf-8
+
+import web
+import json
+import logging
+from gevsubprocess import GPopen as Popen, PIPE, STDOUT
+
+logger = logging.getLogger(__name__)
+
+urls = (
+    '/', 'syncdb',
+)
+
+class syncdb(object):
+    def GET(self):
+        return """\
+POST http://deploy.xiaom.co/syncdb/
+
+Parameters:
+{
+    table1: [addition1, addition2, ..., {column1: define, ...}],
+    table2: [addition1, addition2, ..., {column1: define, ...}],
+    ...
+}
+"""
+
+    def POST(self):
+        data = web.data()
+        #cmd = ['sudo', '-u', 'sheep', '/var/sheep-farm/farm/server/bin/farm-syncdb', data]
+        cmd = ['/var/sheep-farm/farm/server/bin/farm-syncdb', data]
+        p = Popen(cmd, stdout=PIPE, stderr=STDOUT, stdin=open('/dev/null'))
+        for line in p.stdout:
+            logger.debug(line)
+        ret = p.communicate()
+        if ret[1]:
+            yield ret[1]
+        yield 'Syncdb succeeded.'
+
+app_syncdb = web.application(urls, locals())
