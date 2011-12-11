95dd4d55cb916f8de5b9249aff9452ba3cc2687a
CMGS <ilskdw@gmail.com>
2011-12-11 12:07:14 +0800

    bug fix

--- dispatch.py
+++ dispatch.py
@@ -17,7 +17,7 @@ class dispatch(object):
         if not appname:
             return 'Hello Visitor.'
         if appname:
-            ret = load_app_option(appname)
+            ret = load_app_option(appname, 'deploy_servers')
             if ret:
                 return json.dumps(ret.split(','))
         return json.dumps(['deploy'])
