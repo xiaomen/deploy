347f17459fc9778dacef32da5e167084c7db6b99
CMGS <ilskdw@gmail.com>
2011-12-29 14:12:18 +0800

    for one node dpeloy

--- deploy.py
+++ deploy.py
@@ -2,15 +2,15 @@
 
 import socket
 import logging
+import re
 import web
 import time
 
 from gevsubprocess import GPopen as Popen, PIPE, STDOUT
 
-from alloc import get_app_uid
 from syncdb import app_syncdb
 from statics import app_statics
-from dispatch import app_dispatch
+from alloc import get_app_uid, load_app_option
 
 web.config.debug = True
 
@@ -18,8 +18,19 @@ urls = (
     '/', 'deploy',
     '/syncdb', app_syncdb,
     '/statics', app_statics,
-    '/dispatch', app_dispatch,
 )
+
+domain_re = r'http://(.*).xiaom.*'
+RED = '\x1b[01;31m'
+GREEN = '\x1b[01;32m'
+NORMAL = '\x1b[0m'
+
+def render_ok(msg):
+    return GREEN + msg + NORMAL
+
+def render_err(msg):
+    return RED + msg + NORMAL
+
 
 class deploy:
     def GET(self):
@@ -31,14 +42,18 @@ POST http://deploy.xiaom.co/
         # disable nginx buffering
         web.header('X-Accel-Buffering', 'no')
 
-        i = web.input()
+        i = web.input(fast=False)
 
         #get app config if not exist will create it
         app_uid = get_app_uid(i.app_name)
+        servers = get_servers(app_name)
 
-        cmd = ['sudo', '-u', 'sheep', '/usr/local/bin/farm-deploy', i.app_name,
-               i.app_url, str(app_uid)]
+        yield "%d:%s" % (logging.INFO, render_ok(','.join(servers)))
+        servers.remove(get_this(web.ctx['homedomain']))
+
         yield "%d:%s is serving you\n" % (logging.DEBUG, socket.gethostname())
+        cmd = ['sudo', '-u', 'sheep', '/usr/local/bin/farm-deploy', i.app_name,
+               i.app_url, str(app_uid), ]
 
         p = Popen(cmd, stdout=PIPE, stderr=STDOUT, stdin=open('/dev/null'))
         logs = []
@@ -60,6 +75,16 @@ POST http://deploy.xiaom.co/
                                     time.strftime("%H:%M:%S",
                                                   time.localtime(timestamp)),
                                     line)
+
+def get_this(domain):
+    m = re.match(domain_re, domain).groups()
+    return m[0]
+
+def get_servers(appname):
+    ret = load_app_option(appname, 'deploy_servers')
+    if ret:
+        ret = ret.split(',')
+    return []
 
 app = web.application(urls, globals())
 wsgi_app = app.wsgifunc()
